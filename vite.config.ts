bedfb4c6403375516a436a156fdbc1f29206d4f8
mr-react7 <mr007react@gmail.com>
2026-06-10 10:09:13 +0530

    nale-paar-app

--- vite.config.ts
+++ vite.config.ts
@@ -13,14 +13,32 @@
 //     server: { entry: "server" },
 //   },
 // });
+// import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
+// export default defineConfig({
+//   nitro: true,
+
+//   tanstackStart: {
+//     server: {
+//       entry: "server",
+//     },
+//   },
+// });
+
+
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   nitro: true,
-
   tanstackStart: {
     server: {
       entry: "server",
+    },
+  },
+
+  vite: {
+    build: {
+      chunkSizeWarningLimit: 2000,
     },
   },
 });
